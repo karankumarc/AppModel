37a6f91fd8c7782070723a907c9cf1ba81040afe
Karan <karan.kumar.c@gmail.com>
2016-07-19 15:55:56 +0530

    Initial commit

---
+++ app/src/main/java/com/pluralsight/karan/appmodel/MainActivity.java
@@ -0,0 +1,25 @@
+package com.pluralsight.karan.appmodel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
